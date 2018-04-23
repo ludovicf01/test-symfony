5987fb8562cf473d9c932b4f02a68c142c40a500
Lfraval <ludovic.fraval@gmail.com>
2018-04-23 13:51:28 +0200

    gestion du PID

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,17 +2,17 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Patient;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
+    
     public function indexAction()
     {
+        
         // replace this example code with whatever you need
         $content = $this
             ->get('templating')
@@ -23,7 +23,37 @@ class DefaultController extends Controller
     
     public function loadFilesAction()
     {
- 
+        
+        $dir = '..' .DIRECTORY_SEPARATOR. 'web' .DIRECTORY_SEPARATOR. 'files';
+        $files = scandir($dir);
+        for($i = 0;$i < count($files); ++$i){
+            $path = $dir .DIRECTORY_SEPARATOR. $files[$i];
+            $txt_file    = file_get_contents($path);
+            $rows        = explode("\n", $txt_file);
+        
+            foreach($rows as $row => $data)
+            {
+                $patient = new Patient();
+                $em = $this->getDoctrine()->getManager();
+                $row_data = explode('|', $data);
+                if($row_data[0] == "PID"){
+                    $pid5 = explode('^', $row_data[5]);
+                    $patient->setNom($pid5[0]);
+                    $patient->setPrenom($pid5[1]);
+                    $pid7 = explode('^', $row_data[7]);
+                    $patient->setDateNaissance($pid7[0]);   
+                    $pid8 = explode('^', $row_data[8]);
+                    $patient->setGenre($pid8[0]);
+                    $pid11 = explode('^', $row_data[11]);
+                    $patient->setRue($pid11[0]);
+                    $patient->setCodepostal($pid11[4]);
+                    $patient->setVille($pid11[2]);
+                    $em->persist($patient);
+                    $em->flush();
+                }
+            }
+        }
+
         $content = $this
             ->get('templating')
             ->render('AppBundle:Default:result.html.twig');
