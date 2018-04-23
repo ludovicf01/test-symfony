155a0b6f9638d11ba4108a5f5207ffbdca369ddd
Lfraval <ludovic.fraval@gmail.com>
2018-04-23 13:27:11 +0200

    add route&entities, update default controler

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,18 +4,30 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        $content = $this
+            ->get('templating')
+            ->render('AppBundle:Default:index.html.twig');
+
+        return new Response($content);
+    }
+    
+    public function loadFilesAction()
+    {
+ 
+        $content = $this
+            ->get('templating')
+            ->render('AppBundle:Default:result.html.twig');
+
+        return new Response($content);
     }
 }
